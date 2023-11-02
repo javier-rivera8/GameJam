f1830d8d1bfb8f1ac7a7cac108c88ce9a8f0ff93
Javier Rivera <javier.rivera97@upr.edu>
2023-11-02 19:53:16 -0400

    Map

--- scenes/follow cam.gd
+++ scenes/follow cam.gd
@@ -2,12 +2,6 @@ extends Camera2D
 
 @export var tilemap: TileMap
 
-func _ready():
-	var mapRect = tilemap.get_used_rect()
-	var tileSize = tilemap.cell_quadrant_size
-	var worldSizeInPixels = mapRect.size * tileSize
-	limit_right = worldSizeInPixels.x
-	limit_bottom = worldSizeInPixels.y
 
 func _process(delta):
 	pass
